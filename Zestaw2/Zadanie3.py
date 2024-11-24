2c2f6c7738a23703b1fbfe878205a885ac18e7bd
Jakub Pawlikowski <115029954+GetWrong333@users.noreply.github.com>
2024-11-24 20:59:00 +0100

    Update Zadanie3.py

--- Zestaw2/Zadanie3.py
+++ Zestaw2/Zadanie3.py
@@ -5,23 +5,28 @@ def rzymskie_na_arabskie(rzymskie):
         'C': 100, 'D': 500, 'M': 1000
     }
     
-    # Sprawdzanie, czy liczba zawiera tylko prawidłowe znaki
+    #czy liczba zawiera tylko prawidłowe znaki
     dozwolone_znaki = set(mapa_rzymska.keys())
-    if not all(c in dozwolone_znaki for c in rzymskie):
-        raise ValueError("Błąd: Niepoprawny format liczby rzymskiej. Zawiera nieznane symbole.")
     
-    # Sprawdzanie poprawności liczby rzymskiej pod kątem sekwencji
-    if 'IIII' in rzymskie or 'VV' in rzymskie or 'XXXX' in rzymskie or 'LL' in rzymskie or 'CCCC' in rzymskie or 'DD' in rzymskie or 'MMMM' in rzymskie:
-        raise ValueError("Błąd: Niepoprawny format liczby rzymskiej. Zawiera niewłaściwe sekwencje.")
+    # wyjatek gdy napotkamy nieznany symbol
+    for znak in rzymskie:
+        if znak not in dozwolone_znaki:
+            raise ValueError(f"Błąd: Niepoprawny format liczby rzymskiej. Zawiera nieznane symbole: {znak}")
+    
+    # niedozwolone sekwencje
+    niepoprawne_sekwencje = ['IIII', 'VV', 'XXXX', 'LL', 'CCCC', 'DD', 'MMMM', 'IC', 'IL', 'ID', 'IM', 'XD', 'XM']
+    for sekwencja in niepoprawne_sekwencje:
+        if sekwencja in rzymskie:
+            raise ValueError(f"Błąd: Niepoprawny format liczby rzymskiej. Zawiera niewłaściwe sekwencje: {sekwencja}")
     
     wynik = 0
     poprzednia_wartosc = 0
     
-    # Iteracja przez liczby od końca, aby uwzględnić zapisy takie jak IV, IX itd.
+    # Iteracja od końca, aby uwzględnic zapisy jak IV, IX, ...
     for znak in reversed(rzymskie):
         wartosc = mapa_rzymska[znak]
         
-        # Jeśli bieżąca wartość jest mniejsza niż poprzednia, odejmujemy ją
+        # biezaca wartosc jest mniejsza niz poprzednia, to ja odejmujemy 
         if wartosc < poprzednia_wartosc:
             wynik -= wartosc
         else:
@@ -29,11 +34,10 @@ def rzymskie_na_arabskie(rzymskie):
         
         poprzednia_wartosc = wartosc
 
-    # Weryfikacja wyniku przez odwrotną konwersję
-    if arabskie_na_rzymskie(wynik) != rzymskie:
-        raise ValueError("Błąd: Niepoprawny format liczby rzymskiej.")
-
     return wynik
+
+
+
 
 
 def arabskie_na_rzymskie(arabskie):
@@ -54,8 +58,6 @@ def arabskie_na_rzymskie(arabskie):
 
     return rzymska
 
-
-# Sekcja testowa
 if __name__ == '__main__':
     try:
         # Przykłady konwersji rzymskiej na arabską
@@ -65,9 +67,6 @@ if __name__ == '__main__':
         # Przykłady konwersji arabskiej na rzymską
         arabska = 1994
         print(f"Liczba arabska {arabska} to {arabskie_na_rzymskie(arabska)} w rzymskich.")
-        
-        # Test błędu (niepoprawny format)
-        print(rzymskie_na_arabskie("IIII"))  # Powinno zgłosić wyjątek
         
     except ValueError as e:
         print(e)
